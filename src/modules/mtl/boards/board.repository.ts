5d091efa22ea9e8e972e7df5bc79f23e4231578e
yskim6211 <yskim621@mindsai.co.kr>
2025-05-19 17:24:21 +0900

    refactor: updating transaction logic for sample code

--- src/modules/mtl/boards/board.repository.ts
+++ src/modules/mtl/boards/board.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { MindsaiPrismaService } from 'src/prisma/mindsai_platform.prisma.service';
 import { CreateBoardDto, UpdateBoardDto } from './dto/board.dto';
+import { UserDto } from '../user/dto/user.dto';
 
 // 기능과 상관없는 샘플용 코드
 @Injectable()
@@ -12,6 +13,7 @@ export class BoardRepository {
   }
 
   async createMultiData(data: CreateBoardDto) {
+    // 기본 트렌젝션 => 배열 형태로 열거
     return this.prisma.$transaction([this.prisma.board.create({ data }), this.prisma.board.create({ data })]);
   }
 
@@ -26,17 +28,19 @@ export class BoardRepository {
   async update(id: number, data: UpdateBoardDto) {
 
 
+    // 고급 트렌젝션 => 각 트렌잭션 별로 전/후 처리가 가능
     await this.prisma.$transaction(async (tx) => {
       const foundBoardData = await this.findOne(id); // Throws if not found
       if (!foundBoardData) {
         throw new NotFoundException();
       }
-
       foundBoardData.status = contentStatus.HIDDEN;
-      await tx.board.create({ data });
-    });
+      await tx.board.update({ where: { id }, data });
 
-    return this.prisma.board.update({ where: { id }, data });
+      const foundUser = await tx.user.findUnique({ where: { id: foundBoardData.id } });
+      foundUser.name = '휴먼계정';
+      await tx.user.update({ where: { id: foundBoardData.id }, data: foundUser });
+    });
   }
 
   async remove(id: number) {
