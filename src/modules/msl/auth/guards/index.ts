fdd574ddc1e83d843d80f0cc2d544c1f75e2f1a8
yskim621 <yskim62100@gmail.com>
2026-03-16 17:27:14 +0900

    feat: adding basic logics of auth, user, role, permission

---
+++ src/modules/msl/auth/guards/index.ts
@@ -0,0 +1,2 @@
+export { RolesGuard } from './roles.guard';
+export { PermissionsGuard } from './permissions.guard';
--- src/routes/minds-signal.module.ts
+++ src/routes/minds-signal.module.ts
@@ -6,15 +6,16 @@ import { UserModule } from '../modules/msl/user/user.module';
 import { UserController } from '../modules/msl/user/user.controller';
 import { AuthModule } from '../modules/msl/auth/auth.module';
 import { AuthController } from '../modules/msl/auth/auth.controller';
+import { UserGroupModule } from '../modules/msl/user-group/user-group.module';
+import { UserGroupController } from '../modules/msl/user-group/user-group.controller';
+import { PermissionModule } from '../modules/msl/permission/permission.module';
+import { PermissionController } from '../modules/msl/permission/permission.controller';
+import { MenuModule } from '../modules/msl/menu/menu.module';
+import { MenuController } from '../modules/msl/menu/menu.controller';
 
 @Module({
-  imports: [AuthModule, UserModule, BoardsModule],
-  controllers: [AuthController, UserController, BoardsController],
-  providers: [
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: JwtAuthGuard,
-    // },
-  ],
+  imports: [AuthModule, UserModule, UserGroupModule, PermissionModule, MenuModule, BoardsModule],
+  controllers: [AuthController, UserController, UserGroupController, PermissionController, MenuController, BoardsController],
+  providers: [],
 })
 export class MindsSignalModule {}
